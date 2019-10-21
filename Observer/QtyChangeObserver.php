3d7fbe8febaf41c43809132d404965bbe6d570de
Paweł Cieślik <p.cieslik@macopedia.pl>
2019-10-21 10:23:56 +0200

    Add stock synchronization watcher

--- Observer/QtyChangeObserver.php
+++ Observer/QtyChangeObserver.php
@@ -2,58 +2,27 @@
 
 namespace Macopedia\Allegro\Observer;
 
-use Macopedia\Allegro\Model\Configuration;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Macopedia\Allegro\Api\Consumer\MessageInterface;
-use Macopedia\Allegro\Api\Consumer\MessageInterfaceFactory;
-use Magento\Framework\MessageQueue\PublisherInterface;
-use Macopedia\Allegro\Logger\Logger;
-use Magento\Framework\Module\Manager;
 
 /**
  * Product stock change observer
  */
 class QtyChangeObserver implements ObserverInterface
 {
-    const DB_TOPIC_NAME = 'allegro.change.stock.db';
-    const TOPIC_NAME    = 'allegro.change.stock';
-
-    /** @var PublisherInterface  */
-    private $publisher;
-
-    /** @var MessageInterfaceFactory  */
-    private $messageFactory;
-
-    /** @var Logger */
-    private $logger;
-
-    /** @var Configuration */
-    private $config;
-
-    /** @var Manager */
-    private $moduleManager;
+    /**
+     * @var \Macopedia\Allegro\Service\MessageQtyChange
+     */
+    protected $messageQtyChange;
 
     /**
      * QtyChangeObserver constructor.
-     * @param PublisherInterface $publisher
-     * @param MessageInterfaceFactory $messageFactory
-     * @param Logger $logger
-     * @param Manager $moduleManager
-     * @param Configuration $config
+     * @param \Macopedia\Allegro\Service\MessageQtyChange $messageQtyChange
      */
     public function __construct(
-        PublisherInterface $publisher,
-        MessageInterfaceFactory $messageFactory,
-        Logger $logger,
-        Manager $moduleManager,
-        Configuration $config
+        \Macopedia\Allegro\Service\MessageQtyChange $messageQtyChange
     ) {
-        $this->publisher = $publisher;
-        $this->messageFactory = $messageFactory;
-        $this->logger = $logger;
-        $this->moduleManager = $moduleManager;
-        $this->config = $config;
+        $this->messageQtyChange = $messageQtyChange;
     }
 
     /**
@@ -62,35 +31,7 @@ class QtyChangeObserver implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        try {
-
-            if (!$this->config->isStockSynchronizationEnabled()) {
-                return;
-            }
-
-            $productId = $observer->getEvent()->getData('item')->getProductId();
-
-            if (!$productId) {
-                $this->logger->error('Error while getting data from event');
-                return;
-            }
-
-            /** @var MessageInterface $message */
-            $message = $this->messageFactory->create();
-            $message->setProductId($productId);
-
-            if ($this->moduleManager->isEnabled('Magento_Amqp')) {
-                $this->publisher->publish(self::TOPIC_NAME, $message);
-                return;
-            }
-
-            if ($this->moduleManager->isEnabled('Magento_MysqlMq')) {
-                $this->publisher->publish(self::DB_TOPIC_NAME, $message);
-                return;
-            }
-
-        } catch (\Exception $e) {
-            $this->logger->exception($e);
-        }
+        $productId = $observer->getEvent()->getData('item')->getProductId();
+        $this->messageQtyChange->execute($productId);
     }
 }
