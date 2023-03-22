4b0f5dcbaec10a82ecc88738549ec340526dd7ec
PWitMaco <124343174+PWitMaco@users.noreply.github.com>
2023-03-22 10:16:38 +0100

    Add the phpdocs to the pickup point interface in order to fix the magento order endpoint (#104)

--- Api/Data/Sales/Order/PickupPointExtensionAttributesInterface.php
+++ Api/Data/Sales/Order/PickupPointExtensionAttributesInterface.php
@@ -5,16 +5,69 @@ namespace Macopedia\Allegro\Api\Data\Sales\Order;
 interface PickupPointExtensionAttributesInterface
 {
 
+    /**
+     * @param string|null $pointId
+     * @return mixed
+     */
     public function setPointId(?string $pointId);
+
+    /**
+     * @param string|null $name
+     * @return mixed
+     */
     public function setName(?string $name);
+
+    /**
+     * @param string|null $description
+     * @return mixed
+     */
     public function setDescription(?string $description);
+
+    /**
+     * @param string|null $street
+     * @return mixed
+     */
     public function setStreet(?string $street);
+
+    /**
+     * @param string|null $zipCode
+     * @return mixed
+     */
     public function setZipCode(?string $zipCode);
+
+    /**
+     * @param string|null $city
+     * @return mixed
+     */
     public function setCity(?string $city);
+
+    /**
+     * @return string|null
+     */
     public function getPointId(): ?string;
+
+    /**
+     * @return string|null
+     */
     public function getName(): ?string;
+
+    /**
+     * @return string|null
+     */
     public function getDescription(): ?string;
+
+    /**
+     * @return string|null
+     */
     public function getStreet(): ?string;
+
+    /**
+     * @return string|null
+     */
     public function getZipCode(): ?string;
+
+    /**
+     * @return string|null
+     */
     public function getCity(): ?string;
 }
